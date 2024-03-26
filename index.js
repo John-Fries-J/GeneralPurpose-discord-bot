45f679270d683e4fb4877c7cb8721e71360e4877
John-Fries-J <w.bababooie@gmail.com>
2024-03-26 20:29:27 +0000

    remove random comments

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import config from './config.json' assert { type: 'json' };
 
-// start the discord bot with the token from the config file 
 const client = new Discord.Client();
 client.login(config.token);
 console.log('Bot started!')
