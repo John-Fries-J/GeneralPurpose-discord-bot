76eafc76a298f710abce162d534b5a4e3fef8c3e
John-Fries-J <w.bababooie@gmail.com>
2024-03-27 09:21:07 +0000

    fix ticket message

--- commands/ticket/ticketembed.js
+++ commands/ticket/ticketembed.js
@@ -1,21 +1,24 @@
-const { MessageEmbed, Permissions } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { red } = require('../../colors.json');
 const config = require('../../config.json');
-//need to create a ticket channel in the config.json file
 
 module.exports = {
-    name: 'ticket',
-    description: 'Create a ticket embed with a button to open a ticket',
-    async execute(message, args) {
-        const ticketEmbed = new MessageEmbed()
-            .setColor('#0099ff')
+    data: new SlashCommandBuilder()
+        .setName('ticket')
+        .setDescription('Create a ticket embed with a button to open a ticket'),
+    async execute(interaction) {
+        const ticketEmbed = new EmbedBuilder()
             .setTitle('Ticket')
             .setDescription('Click the button below to open a ticket.')
-            .addField('Ticket Creator', message.author.tag)
+            .setColor(red)
             .setTimestamp();
 
-        const ticketchannel = config.ticketChannel
-        const channel = ticketchannel
-        const ticketMessage = await message.channel.send({ embeds: [ticketEmbed] });
+
+        const ticketChannelId = config.ticketChannel;
+        const channel = interaction.guild.channels.cache.get(ticketChannelId);
+        const ticketMessage = await channel.send({ embeds: [ticketEmbed] });
+
+        interaction.reply({ content: `Ticket embed has been sent to <#${ticketChannelId}>`, ephemeral: true });
 
         ticketMessage.react('🎫');
         // need to make it a button but its a good start. The button will need to be made in or handled in the events folder anyway. Cant do on laptop.
