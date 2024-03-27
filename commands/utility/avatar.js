0c8a7b2acab3f0893f55e33734da589ef3451a92
John-Fries-J <w.bababooie@gmail.com>
2024-03-27 05:30:24 +0000

    create avatar command

---
+++ commands/utility/avatar.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { blue } = require('../../colors.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Provides the avatar of the user or mentioned user.')
+        .addUserOption(option => option.setName('user').setDescription('The user to get the avatar of.')),
+    async execute(interaction) {
+        let user = interaction.options.getUser('user') || interaction.user;
+        let mentionedUser = interaction.options.getMember('user');
+        
+        const embed = new EmbedBuilder()
+            .setColor(blue)
+            .setTimestamp();
+
+        if (mentionedUser) {
+            user = mentionedUser.user;
+            embed.setTitle(`${user.tag}'s Avatar`);
+        } else {
+            embed.setTitle(`${interaction.user.tag}'s Avatar`);
+        }
+        
+        embed.setImage(user.displayAvatarURL({ dynamic: true }));
+        
+        await interaction.reply({ embeds: [embed] });
+    },
+};
