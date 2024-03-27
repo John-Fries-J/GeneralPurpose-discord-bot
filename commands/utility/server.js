1ff7735b233511e2d0374141bf0caa4252df2d08
John-Fries-J <w.bababooie@gmail.com>
2024-03-27 04:20:58 +0000

    Make the Server Command significantly better

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -1,10 +1,16 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { blue } = require('../../colors.json');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('server')
 		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+		const embed = new EmbedBuilder()
+		.setTitle('Server Info')
+		.setDescription(`**Server's name:** ${interaction.guild.name} *(ID: ${interaction.guild.id})*\n**Server Owner:** <@${interaction.guild.ownerId}> \n**Members:** ${interaction.guild.memberCount}\n**Created:** ${interaction.guild.createdAt.toLocaleString('en-GB', {timeZone: 'GMT', day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short'})}`)		.setColor(blue)
+		.setTimestamp()
+		.setThumbnail(interaction.guild.iconURL());
+		await interaction.reply({ embeds: [embed] });
 	},
 };
