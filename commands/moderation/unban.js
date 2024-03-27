fc32d1cb6e2bd4ce656a95236807239b50f9656e
John-Fries-J <w.bababooie@gmail.com>
2024-03-27 04:26:01 +0000

    Create an unban command.

---
+++ commands/moderation/unban.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('unban')
+        .setDescription('Unbans a user from the server.')
+        .addUserOption(option => option.setName('user').setDescription('The user to unban').setRequired(true)),
+    async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        const member = await interaction.guild.members.unban(user);
+        await interaction.reply(`${member.tag} has been unbanned.`);
+    },
+};
