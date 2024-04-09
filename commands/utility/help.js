ffafac2cce4977333ea319f4d408522558a0ae94
John Fries <w.bababooie@gmail.com>
2024-04-09 13:12:28 +0100

    Alter to work and have args

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,41 +1,49 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
 const { blue } = require('../../colors.json');
 const fs = require('fs');
 const path = require('path');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('help')
-        .setDescription('Shows all available commands'),
+        .setDescription('Shows all available commands')
+        .addStringOption(option => option.setName('command').setDescription('The command to get help for')),
     async execute(interaction) {
+        if (interaction.options.getString('command')) {
+            const commandName = interaction.options.getString('command');
+            const command = interaction.client.commands.get(commandName);
+
+            if (!command) {
+                return await interaction.reply({ content: 'That command does not exist.', ephemeral: true });
+            }
+
+            const embed = new EmbedBuilder()
+                .setTitle(`Help for **${commandName}** command`)
+                .setDescription(`**Description:**\n${command.data.description}`)
+                .setColor(blue)
+                .setTimestamp();
+
+            await interaction.reply({ embeds: [embed] });
+            return;
+        }else{
+
+        const commandCategories = fs.readdirSync("./commands").filter(file => fs.lstatSync(path.join("./commands", file)).isDirectory());
         const embed = new EmbedBuilder()
-        .setTitle('Help 📚')
-        .setDescription('Here is a list of all available commands')
-        .setColor(blue)
-        .setTimestamp();
+            .setTitle('Help 📚')
+            .setColor(blue)
+            .setTimestamp();
 
-        const commmandCategries = fs.readdirSync(path.join(__dirname, '../../commands'), {withFileTypes: true })
-        .filter(dir => dir.isDirectory())
-        .map(dir => dir.name);
+        commandCategories.forEach(category => {
+            const categoryCommands = fs.readdirSync(`./commands/${category}`).filter(file => file.endsWith('.js'));
+            const commands = categoryCommands.map(command => {
+                const commandName = require(`../${category}/${command}`).data.name;
+                return commandName;
+            });
+            embed.addFields({ name: category.charAt(0).toUpperCase() + category.slice(1), value: commands.join(', ') });
+        });
 
-        const getCommandsFromDirectory = (directory) => {
-            const commandFiles = fs.readdirSync(directory).filter(file => file.endsWith('.js'));
-            (async () => {
-                for (const file of commandFiles) {
-                    const command = require(path.join(directory, file));
-                    if (command.data && command.data instanceof SlashCommandBuilder) {
-                        const { name, description } = command.data.toJSON();
-                        embed.addField(`/${name}`, description);
-                    }
-                }
-                for (const category of commandCategories) {
-                    embed.addField(`**${category}**`, '\u200B'); // Add category title
-                    const categoryDirectory = path.join(__dirname, `../../commands/${category}`);
-                    getCommandsFromDirectory(categoryDirectory); // Fetch commands for the category
-                }
-
-                await interaction.reply({ embeds: [embed] });
-            })();
+        await interaction.reply({ embeds: [embed] });
     }
     },
 };
