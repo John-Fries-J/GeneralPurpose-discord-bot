ca0fb903411cd03deccdddb59ea3d746ab696894
John Fries <w.bababooie@gmail.com>
2024-04-22 18:07:13 +0100

    also checks for "logs"

--- events/logging.js
+++ events/logging.js
@@ -5,17 +5,22 @@ const config = require('../config.json');
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
-            const channelId = `${config.logChannel}`;
-            const channel = interaction.guild.channels.cache.get(channelId);
-            if (!channel) return console.log('Log channel not found');
-            if (interaction.isButton()) {
-                const logEmbed = new EmbedBuilder()
+        const channelId = `${config.logChannel}`;
+        const channel = interaction.guild.channels.cache.get(channelId) || interaction.guild.channels.cache.find(channel => channel.name === 'logs');
+        
+        if (!channel) {
+            console.log('Log channel not found');
+            return;
+        }
+
+        if (interaction.isButton()) {
+            const logEmbed = new EmbedBuilder()
                 .setTitle(`${interaction.user.tag} Clicked a button`)
                 .setDescription(`${interaction.user.tag} clicked a button in ${interaction.channel}`)
                 .setColor(blue)
                 .setTimestamp();
             channel.send({ embeds: [logEmbed] });
-            } else{
+        } else {
             const logEmbed = new EmbedBuilder()
                 .setTitle(`${interaction.user.tag} ran a command`)
                 .setDescription(`Command ran in ${interaction.channel}, by ${interaction.user.tag}.\nCommand: ${interaction.commandName}\n[Go there](https://discord.com/channels/${interaction.guild.id}/${interaction.channel.id}/${interaction.id})`)
@@ -23,5 +28,5 @@ module.exports = {
                 .setTimestamp();
             channel.send({ embeds: [logEmbed] });
         }
-        }
+    }
 };
