d2cec852db1bc249a99eda74c91cda103a820ca4
John Fries <w.bababooie@gmail.com>
2024-09-15 13:03:30 +0100

    send rdy message when bot is online

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,24 @@
-const { Client, Intents } = require('discord.js');
-const { statusName } = require('../config.json');
+const { Client, Intents, embedBuilder } = require('discord.js');
+const { statusName, logChannels } = require('../config.json');
+const { green } = require('../colors.json');
 
 module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
+        const channelId = logChannels.logChannel;
+        const channel = client.channels.cache.get(channelId);
+        if (!channel) {
+            console.log('Log channel not found');
+            return;
+        }
+        const logEmbed = new EmbedBuilder()
+            .setTitle('Bot is ready')
+            .setDescription('Bot is now online and ready to use')
+            .setColor(blue)
+            .setTimestamp();
+        channel.send({ embeds: [logEmbed] });
         if (!statusName) return console.log('No status name provided in config.json')
         else {
         client.user.setPresence({ activities: [{ name: `${statusName}` }] });
