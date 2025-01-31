b85d3abca894a5294b8e185239b47f1d9cf7ec2d
John Fries <w.bababooie@gmail.com>
2025-01-31 03:04:01 +0000

    Upd: Add new console logging

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers] });
 client.commands = new Collection();
+
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
+
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
+			console.log(`[COMMAND] /${command.data.name}`);
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
@@ -27,14 +32,19 @@ const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
+
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
+		console.log(`[EVENT] ${event.name} (once)`);
 	} else {
 		client.on(event.name, (...args) => event.execute(...args));
+		console.log(`[EVENT] ${event.name}`);
 	}
 }
+
 if (!token) {
 	console.log('[ERROR] No token provided in config.json');
 	process.exit(1);
 }
+
 client.login(token);
